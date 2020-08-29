e49b8609c7d08460231a8efdab6ed8da6c182465
Mr.H <823527728@qq.com>
2020-08-29 18:54:42 +0800

    Update getver.sh

--- scripts/getver.sh
+++ scripts/getver.sh
@@ -12,7 +12,7 @@ try_version() {
 }
 
 try_git() {
-	REBOOT=656113b77abb73750d19cc9b6fc1cdb68ecefa85
+	REBOOT=97a4ffcc125611dd5f307d54570373832a73e62d
 	git rev-parse --git-dir >/dev/null 2>&1 || return 1
 
 	[ -n "$GET_REV" ] || GET_REV="HEAD"
